33d08408831770ee7216fb5c99445ab246e614b6
Hugo Vizcaino <hugo@darwinex.com>
2024-10-16 18:57:16 +0200

    give some basic styling to the list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { getAllCountries } from "@/data/rest-countries";
 import styles from "./page.module.css";
-
-export default function Home() {
+import Image from "next/image";
+export default async function Home() {
+  const countries = await getAllCountries();
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <main>
         <h1>S_PTecn_D_SFact24_G</h1>
+        <ul className={styles.countries_list}>
+          {countries.map((country) => (
+            <li key={country.name.official} className={styles.country_item}>
+              <Image src={country.flags.svg} alt={country.name.official} width={32} height={32} />
+              <span>{country.name.official}</span>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
